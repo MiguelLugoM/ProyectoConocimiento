bf4e245fe759036a1e003206d548e1e9e4109f8b
Miguel Lugo <miguellugo315651@correo.itm.edu.co>
2024-10-12 19:15:40 -0500

    Se actualiza el codigo

--- ProyectoBackendCsharp/obj/Debug/net8.0/ProyectoBackendCsharp.AssemblyInfo.cs
+++ ProyectoBackendCsharp/obj/Debug/net8.0/ProyectoBackendCsharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBackendCsharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69bd89a295bfd507b9b64095aec43de431260a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6224c038d9b36354e88030342841b487cf0d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBackendCsharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBackendCsharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
